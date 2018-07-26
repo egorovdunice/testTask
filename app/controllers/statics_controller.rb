e72fa9a71aa95361b7550b493eef53ba82d79a63
alakril <a.egorov@dunice.net>
2018-07-26 15:44:27 +0300

    Added devise gem to work with user

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,5 @@
+# wut
+
+class StaticsController < ApplicationController
+  def home; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'statics#home'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
